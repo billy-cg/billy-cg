0fbc7f9db9a5b609a172cd0a38f466ad1f469302
billy-cg <guillermocaratini@gmail.com>
2025-05-17 15:01:19 -0300

    Add files via upload

---
+++ secuencial c++/ejer1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/**
+  Hacer un programa para ingresar por teclado la cantidad de horas trabajadas por un operario
+  y el valor que se le paga por hora trabajada y listar por pantalla el sueldo que le corresponda.
+
+*/
+
+
+int main(){
+    int horas, precioHora, sueldo;
+
+    cout << "ingrese horas trabajadas:" ;
+    cin >> horas;
+    cout << "ingrese precio por hora:" ;
+    cin >> precioHora;
+
+    sueldo=horas*precioHora;
+
+
+    cout << "su sueldo es:" << sueldo << endl;
+
+    return 0;
+}
